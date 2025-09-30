d97bb17c2c0991a71eff4428ec1aaa46dfb59407
e42240950-RafifDwiPradipta <e42240950@student.polije.ac.id>
2025-09-30 13:32:30 +0700

    Latihan_PHP

---
+++ tugas perulangan.php
@@ -0,0 +1,8 @@
+<?php
+$angka = 100; // Inisialisasi nilai awal di luar loop
+
+while ($angka <= 1000) { // Kondisi diperiksa di awal
+  echo $angka . "<br>";
+  $angka++; // Increment (penambahan nilai) dilakukan di dalam loop
+}
+?>
